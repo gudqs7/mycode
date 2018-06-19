304b53b0a8c83e2fd5231c532c944525b7c575b4
wq <guddqs@icloud.com>
2018-06-19 18:10:49 +0800

    完善 sed -i 判断

--- zsh/install_zsh.sh
+++ zsh/install_zsh.sh
@@ -2,4 +2,8 @@ yum install zsh git -y
 wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
 chsh -s /bin/zsh
 home=`echo ~`
-sed -i "" 's/robbyrussell/af-magic/g' $home/.zshrc 
+if [ -f /etc/issue ]; then
+  sed -i 's/robbyrussell/af-magic/g' $home/.zshrc
+else
+  sed -i "" 's/robbyrussell/af-magic/g' $home/.zshrc
+fi
