76773d7613d413f0734870dbeafe4e588de0624b
gudqs7 <guddqs@icloud.com>
2019-04-08 17:45:13 +0800

    zsh_higglinght bug fix

--- zsh/zsh_highlight_centos.sh
+++ zsh/zsh_highlight_centos.sh
@@ -1,4 +1,4 @@
 source ~/.zshrc
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-sed -i 's/  git/git  zsh-syntax-highlighting/g' ~/.zshrc
+sed -i 's/(git/(git  zsh-syntax-highlighting/g' ~/.zshrc
 source ~/.zshrc
