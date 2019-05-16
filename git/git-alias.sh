acd58bf9f051934091f23327ec977cf1638c8c61
wq <guddqs@icloud.com>
2019-05-16 17:37:45 +0800

    git-alias

---
+++ git/git-alias.sh
@@ -0,0 +1,9 @@
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --global alias.cocheckout
+git config --global alias.ci commit
+git config --global alias.brbranch
+git config --global alias.ststatus
+git config --global alias.unstage 'reset HEAD'
+git config --global alias.last 'log -1'
+
+git config --global alias.lw "log --pretty=format:'%cd--%s--%an--%h' --date=format:%Y-%m-%d"
